abcd77bbddf7c7fccc76ac160c957825a236b13d
R1013-T <nosuke1013@icloud.com>
2023-08-24 13:24:05 +0900

    custom schema init

---
+++ server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"go_gql/graph"
+	"go_gql/internal"
+	"log"
+	"net/http"
+	"os"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(internal.NewExecutableSchema(internal.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
